ce27b87aa628c99b107ccf3cb3a83823f0adf1a1
sajedazoabi <zsajeda133@gmail.com>
2017-07-11 16:40:12 +0300

    add to do function relates #9

--- test.js
+++ test.js
@@ -12,7 +12,84 @@ test('Example test', function(t) {
 // up to here //
 
 // tests for addTodo function //
+test('check it returns unchanged todos object', function(t) {
+  var todos = [{
+      id: 0,
+      description: 'smash avocados',
+      done: true
+    },
+    {
+      id: 1,
+      description: 'make coffee',
+      done: false
+    }
+  ];
 
+  var actual = logic.todoFunctions.addTodo(todos)
+  var expected = todos
+  t.equal(actual, expected);
+  t.end();
+})
+
+test('check add newtodo',function(t){
+  var newTodo = {
+  id: 2,
+  description:'wash dishes',
+  done: true
+}
+var todos = [{
+    id: 0,
+    description: 'smash avocados',
+    done: true
+  },
+  {
+    id: 1,
+    description: 'make coffee',
+    done: false
+  }
+];
+  var actual = logic.todoFunctions.addTodo(todos,newTodo);
+  var expected = todos.concat(newTodo);
+  t.deepEqual(actual, expected);
+  t.end();
+
+})
+
+test('check it has new id', function(t) {
+  var todos = [{
+      id: 0,
+      description: 'smash avocados',
+      done: true
+    },
+    {
+      id: 1,
+      description: 'make coffee',
+      done: false
+    }
+  ];
+
+  var newTodo = {description: "Help I can't code"}
+
+  var actual = logic.todoFunctions.addTodo(todos, newTodo)
+  var expected = [{
+      id: 0,
+      description: 'smash avocados',
+      done: true
+    },
+    {
+      id: 1,
+      description: 'make coffee',
+      done: false
+    },
+    {
+      id: 2,
+      description: "Help I can't code",
+      done: false
+    }
+  ]
+  t.deepEqual(actual, expected);
+  t.end();
+})
 
 // up to here //
 
